b51ff593f8de1c5d929e20f7c3de9e4a4d776491
Nalzurin <alexnormandi@ukr.net>
2024-10-02 22:44:56 +0300

    Improve bot AI

--- src/Scripts/BotIntelligence.ts
+++ src/Scripts/BotIntelligence.ts
@@ -3,7 +3,7 @@ import { takeMatches } from "./GameLogic";
 
 export function botTurn(currentMatchesCount: number, botMatches: number) {
   // Bot takes between 1 and 3 matches, depending on how many are left
-  const matchesToTake = Math.min(currentMatchesCount, 3);
+  const matchesToTake: number = optimalMove(currentMatchesCount, botMatches);
 
   return takeMatches(
     matchesToTake,
@@ -12,4 +12,21 @@ export function botTurn(currentMatchesCount: number, botMatches: number) {
     0,
     botMatches
   );
+}
+function optimalMove(currentMatchesCount: number, botMatches: number): number {
+  if (currentMatchesCount <= 3) {
+    for (let i = 1; i < currentMatchesCount + 1; i++) {
+      if ((botMatches + i) % 2 == 0) {
+        return i;
+      }
+    }
+    return currentMatchesCount;
+  } else {
+    for (let i = 1; i < 4; i++) {
+      if ((botMatches + i) % 2 == 0) {
+        return i;
+      }
+    }
+    return 1;
+  }
 }
